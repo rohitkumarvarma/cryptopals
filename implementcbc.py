f66f3c45c41806bf3cc36626feb3035a52e4108b
rohitkumarvarma <53017078+rohitkumarvarma@users.noreply.github.com>
2019-12-26 00:09:49 +0530

    Add files via upload

---
+++ implementcbc.py
@@ -0,0 +1,32 @@
+from Crypto.Cipher import AES
+key = "YELLOW SUBMARINE"
+ctlist = []
+iv = "\x00" * 16
+f = open("10.txt","r")
+k = f.readlines()
+s ="".join(k)
+def AES_decrypt(ct, key):
+	cipher = AES.new(key, AES.MODE_ECB)	
+	pt=cipher.decrypt(ct)
+	return(pt)
+def supplyct(s):
+    g=-16
+    k=0
+
+    for i in range (len(s)//16):
+        g = g+16
+        k = k+16
+        s1 = ""
+        for j in range (g,k):
+            s1 = s1+s[j]
+        ctlist.append(s1)
+    return (ctlist)
+def plaintext(s):
+    mylist = print(supplyct(s))
+    for i in range (len(mylist)):
+        if i==0:
+            pt1 = AES_decrypt(s[0], key)
+            
+            
+            
+            
